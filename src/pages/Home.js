97dc2937d799684f89604019e85ca78ae7a2cbe4
admin <admin@admins-MacBook-Air.local>
2024-05-09 11:51:18 -0700

    update page Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,27 @@
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
-export function APIgetTrendMovies() {
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZTE3MzI1MTIwZDBjMTg2MWVlOWY1Y2E5OTU5M2UyNyIsInN1YiI6IjY1YTUyMDc0MDU4MjI0MDEyOWZkZWI3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.02VK07NSdM1DVG9zE8VFFZ__KEaYfqoTTfKCjcVdyXg',
-    },
-  };
-
-  fetch(
-    'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
-    options
-  )
-    .then(response => response.json())
-    .then(response => <HomePage data={response.results} />)
-    .catch(err => console.error(err));
-}
-
 function HomePage() {
-  const [dataTrendMovies, setDataTrendMovies] = useState([]);
+  const [listMovies, setListMovies] = useState([]);
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZTE3MzI1MTIwZDBjMTg2MWVlOWY1Y2E5OTU5M2UyNyIsInN1YiI6IjY1YTUyMDc0MDU4MjI0MDEyOWZkZWI3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.02VK07NSdM1DVG9zE8VFFZ__KEaYfqoTTfKCjcVdyXg',
+      },
+    };
+
+    fetch(
+      'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
+      options
+    )
+      .then(response => response.json())
+      .then(response => setListMovies(response.results))
+      .catch(err => console.error(err));
+  }, []);
+
   useEffect(() => {}, []);
   return (
     <>
